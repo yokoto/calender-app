f0eefb6c21fbb0c21a294bd071da11122dab4f2e
yokoto <yo44ko55@gmail.com>
2020-05-02 13:37:41 +0900

    "-" が "+" になっていたので修正

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -5,7 +5,7 @@ const Counter = ({ count, increment, decrement }) => (
   <>
     <div>{count}</div>
     <button onClick={() => increment(1)}>+</button>
-    <button onClick={() => decrement(1)}>+</button>
+    <button onClick={() => decrement(1)}>-</button>
   </>
 );
 
